4f0ac80b3a4b6ee090bd102e0220ee2fe874a017
zeem2626 <zeem1920@gmail.com>
2024-02-06 22:31:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+   const [count, setCount] = useState(0);
+   const [videos, setVideos] = useState([]);
+   const getVideos = async () => {
+      const res = await fetch(
+         "https://youtube-r5sb.onrender.com/api/video/random"
+      );
+      const data = await res.json();
+      console.log(data.data);
+      setVideos(data.data);
+   };
+
+   useEffect(() => {
+      getVideos();
+   }, []);
+
+   return (
+      <>
+         <div>
+            <a href="https://vitejs.dev" target="_blank">
+               <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+               <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+         </div>
+         <h1>Vite + React</h1>
+         <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+               count is {count}
+            </button>
+            <button onClick={getVideos}>Get Videos
+            </button>
+         </div>
+         <div>
+          {videos.map((elem)=> <h2>{elem.title}</h2>)}
+         </div>
+      </>
+   );
+}
+
+export default App;
